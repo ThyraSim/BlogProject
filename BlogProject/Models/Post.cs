83802e8d322a1bcbd95c3888098352bda808feb2
Tim <roppa070@gmail.com>
2023-09-09 12:55:24 -0400

    Modified feed page to include post creation when logged in. Put the post creation in a component, client side validation now working

--- BlogProject/Models/Post.cs
+++ BlogProject/Models/Post.cs
@@ -1,12 +1,17 @@
-﻿namespace BlogProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogProject.Models
 {
     public class Post
     {
         public int Id { get; set; }
+        [Required]
         public string UserId { get; set; }
         public User User { get; set; }
         public List<Comment> Comments { get; set; }
+        [Required]
         public string Titre { get; set; }
+        [Required]
         public string Body { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
---
+++ BlogProject/ViewComponents/PostFormViewComponent.cs
@@ -0,0 +1,14 @@
+﻿namespace BlogProject.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+    using BlogProject.Models;
+
+    public class PostFormViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(new Post());
+        }
+    }
+
+}
